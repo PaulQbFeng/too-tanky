145cc71fc0b0d04cbab7fe3166074ec55dbd1511
nbzpro <specialzhdav@gmail.com>
2022-11-12 12:20:19 +0100

    pog.py sort la liste des skins de Annie

--- pog.py
+++ pog.py
@@ -1,1 +1,14 @@
-1+1
+import json
+
+#open the json file
+file = open("data/1.json", encoding="utf8")
+
+#json to dict
+Annie = json.load(file)
+
+#list of skins
+skin_list=[]
+for i in range(len(Annie['skins'])-1):
+    skin_list.append(Annie['skins'][i]['name'])
+
+print(skin_list)
