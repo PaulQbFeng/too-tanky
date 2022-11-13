cc7e3b3024fe3f8a5c0a400a3b5111c4302de3f4
paulqbfeng <paul.feng@axa.com>
2022-11-13 23:17:01 +0100

    Add ahri lvl 18 test stats

--- test_champion.py
+++ test_champion.py
@@ -1,7 +1,16 @@
-from champion import Annie, Ahri
+from champion import Ahri, Annie
+
 
 def test_auto_attack_lvl1():
     annie = Annie()
     ahri = Ahri()
     assert round(annie.auto_attack(ahri), 2) == 42.37
     assert round(ahri.auto_attack(annie), 2) == 44.54
+
+
+def test_ahri_stat_lvl18():
+    ahri = Ahri(level=18)
+    assert ahri.hp == 2202
+    assert ahri.mp == 843
+    assert ahri.attackdamage == 104
+    assert round(ahri.attackspeed, 3) == 0.895
