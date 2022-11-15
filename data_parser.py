c0cd28ae6abbc01a942654e98218c69b13776ed1
nbzpro <specialzhdav@gmail.com>
2022-11-16 00:42:10 +0100

    small fixes

--- data_parser.py
+++ data_parser.py
@@ -19,8 +19,22 @@ def get_dataset_from_json(filename : str):
 def fill_champion_stats(dataset: dict):
     return { name: data["stats"] for name, data in dataset["data"].items() }
 
+def fill_item_stats(item_set: dict):
+    """{item_name : {stat_name : stat_value}}"""
+    ALL_ITEM_STATS = dict()
+    for item_id in item_set["data"]:
+        item = item_set["data"][item_id]
+        item["stats"].update({"gold" : item["gold"]["total"]})
+        ALL_ITEM_STATS[item["name"]] = item["stats"]
+    return ALL_ITEM_STATS
+
+
 dataset = get_dataset_from_json("data/ddragon/champion.json")
 ALL_CHAMPION_BASE_STATS = fill_champion_stats(dataset)
+
+item_set = get_dataset_from_json("data/ddragon/item.json")
+ALL_ITEM_STATS = fill_item_stats(item_set)
+
 
 SCALING_STAT_NAMES = [
     "hp",
