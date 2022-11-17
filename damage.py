862925ffc5856a30d62622739c0193d160302d9e
LinSun00 <91116220+LinSun00@users.noreply.github.com>
2022-11-17 13:24:59 +0100

    physical_damage_after_armor combines the positive armor and negative armor cases that were identical for all damage functions

--- damage.py
+++ damage.py
@@ -1,5 +1,6 @@
 # TODO: it seems that hp are ceiled while damage floored. (however it seems that the ad is rounded in the stat section ingame)
 
+
 def physical_damage_after_positive_armor(pre_mitigation_damage: float, defense_armor: float):
     """
     Calculates the output damage if X amount of pre-mitigation physical damage is dealt to a champion with Y amount of
@@ -16,6 +17,20 @@ def physical_damage_after_negative_armor(pre_mitigation_damage: float, defense_a
     return pre_mitigation_damage * (2 - 100 / (100 - defense_armor))
 
 
+def physical_damage_after_armor(pre_mitigation_damage: float, attack_lethality: float, attack_level: int,
+                                attack_armor_pen_mult_factor: float, attack_bonus_armor_pen_mult_factor: float,
+                                defense_base_armor: float, defense_bonus_armor: float):
+    defense_armor = defense_base_armor + defense_bonus_armor
+    if defense_armor < 0:
+        return physical_damage_after_negative_armor(pre_mitigation_damage, defense_armor)
+    else:
+        attack_flat_armor_pen = attack_lethality * (0.6 + 0.4 * attack_level / 18)
+        armor_eq = defense_base_armor * attack_armor_pen_mult_factor + defense_bonus_armor * attack_armor_pen_mult_factor * attack_bonus_armor_pen_mult_factor
+        armor_eq = armor_eq - attack_flat_armor_pen
+        armor_eq = max(armor_eq, 0)
+        return physical_damage_after_positive_armor(pre_mitigation_damage, armor_eq)
+
+
 def damage_normal_auto_attack_no_crit(attack_base_ad: float, attack_bonus_ad: float, attack_lethality: float,
                                       attack_level: int, attack_armor_pen_mult_factor: float,
                                       attack_bonus_armor_pen_mult_factor: float, defense_base_armor: float,
@@ -26,15 +41,9 @@ def damage_normal_auto_attack_no_crit(attack_base_ad: float, attack_bonus_ad: fl
     percentage armor reduction resulting from spells like Garen E, Trundle R, Olaf Q, Corki E, or items like Black Cleaver
     """
     pre_mitigation_damage = attack_base_ad + attack_bonus_ad
-    defense_armor = defense_base_armor + defense_bonus_armor
-    if defense_armor < 0:
-        return physical_damage_after_negative_armor(pre_mitigation_damage, defense_armor)
-    else:
-        attack_flat_armor_pen = attack_lethality * (0.6 + 0.4 * attack_level / 18)
-        armor_eq = defense_base_armor * attack_armor_pen_mult_factor + defense_bonus_armor * attack_armor_pen_mult_factor * attack_bonus_armor_pen_mult_factor
-        armor_eq = armor_eq - attack_flat_armor_pen
-        armor_eq = max(armor_eq, 0)
-        return physical_damage_after_positive_armor(pre_mitigation_damage, armor_eq)
+    return physical_damage_after_armor(pre_mitigation_damage, attack_lethality, attack_level,
+                                       attack_armor_pen_mult_factor, attack_bonus_armor_pen_mult_factor,
+                                       defense_base_armor, defense_bonus_armor)
 
 
 def damage_normal_auto_attack_with_crit(attack_base_ad: float, attack_bonus_ad: float, attack_bonus_crit_damage: float,
@@ -47,15 +56,9 @@ def damage_normal_auto_attack_with_crit(attack_base_ad: float, attack_bonus_ad: 
     percentage armor reduction resulting from spells like Garen E, Trundle R, Olaf Q, Corki E, or items like Black Cleaver
     """
     pre_mitigation_damage = (attack_base_ad + attack_bonus_ad) * (1.75 + attack_bonus_crit_damage)
-    defense_armor = defense_base_armor + defense_bonus_armor
-    if defense_armor < 0:
-        return physical_damage_after_negative_armor(pre_mitigation_damage, defense_armor)
-    else:
-        attack_flat_armor_pen = attack_lethality * (0.6 + 0.4 * attack_level / 18)
-        armor_eq = defense_base_armor * attack_armor_pen_mult_factor + defense_bonus_armor * attack_armor_pen_mult_factor * attack_bonus_armor_pen_mult_factor
-        armor_eq = armor_eq - attack_flat_armor_pen
-        armor_eq = max(armor_eq, 0)
-        return physical_damage_after_positive_armor(pre_mitigation_damage, armor_eq)
+    return physical_damage_after_armor(pre_mitigation_damage, attack_lethality, attack_level,
+                                       attack_armor_pen_mult_factor, attack_bonus_armor_pen_mult_factor,
+                                       defense_base_armor, defense_bonus_armor)
 
 
 def avg_damage_normal_auto_attack(attack_base_ad: float, attack_bonus_ad: float, attack_crit_chance: float,
@@ -69,15 +72,10 @@ def avg_damage_normal_auto_attack(attack_base_ad: float, attack_bonus_ad: float,
     """
     pre_mitigation_damage = (attack_base_ad + attack_bonus_ad) * (
             1 + attack_crit_chance * (0.75 + attack_bonus_crit_damage))
-    defense_armor = defense_base_armor + defense_bonus_armor
-    if defense_armor < 0:
-        return physical_damage_after_negative_armor(pre_mitigation_damage, defense_armor)
-    else:
-        attack_flat_armor_pen = attack_lethality * (0.6 + 0.4 * attack_level / 18)
-        armor_eq = defense_base_armor * attack_armor_pen_mult_factor + defense_bonus_armor * attack_armor_pen_mult_factor * attack_bonus_armor_pen_mult_factor
-        armor_eq = armor_eq - attack_flat_armor_pen
-        armor_eq = max(armor_eq, 0)
-        return physical_damage_after_positive_armor(pre_mitigation_damage, armor_eq)
+    return physical_damage_after_armor(pre_mitigation_damage, attack_lethality, attack_level,
+                                       attack_armor_pen_mult_factor, attack_bonus_armor_pen_mult_factor,
+                                       defense_base_armor, defense_bonus_armor)
+
 
 
 def damage_empowered_auto_attack_no_crit(attack_base_ad: float, attack_bonus_ad: float, attack_lethality: float,
@@ -92,15 +90,10 @@ def damage_empowered_auto_attack_no_crit(attack_base_ad: float, attack_bonus_ad:
     percentage armor reduction resulting from spells like Garen E, Trundle R, Olaf Q, Corki E, or items like Black Cleaver
     """
     pre_mitigation_damage = attack_base_ad * (1 + percentage_AD_increase) + attack_bonus_ad * (1 + percentage_AD_increase) * (1 + percentage_bonus_AD_increase) + flat_AD_increase
-    defense_armor = defense_base_armor + defense_bonus_armor
-    if defense_armor < 0:
-        return physical_damage_after_negative_armor(pre_mitigation_damage, defense_armor)
-    else:
-        attack_flat_armor_pen = attack_lethality * (0.6 + 0.4 * attack_level / 18)
-        armor_eq = defense_base_armor * attack_armor_pen_mult_factor + defense_bonus_armor * attack_armor_pen_mult_factor * attack_bonus_armor_pen_mult_factor
-        armor_eq = armor_eq - attack_flat_armor_pen
-        armor_eq = max(armor_eq, 0)
-        return physical_damage_after_positive_armor(pre_mitigation_damage, armor_eq)
+    return physical_damage_after_armor(pre_mitigation_damage, attack_lethality, attack_level,
+                                       attack_armor_pen_mult_factor, attack_bonus_armor_pen_mult_factor,
+                                       defense_base_armor, defense_bonus_armor)
+
 
 
 def damage_empowered_auto_attack_with_crit(attack_base_ad: float, attack_bonus_ad: float,
@@ -115,15 +108,10 @@ def damage_empowered_auto_attack_with_crit(attack_base_ad: float, attack_bonus_a
     percentage armor reduction resulting from spells like Garen E, Trundle R, Olaf Q, Corki E, or items like Black Cleaver
     """
     pre_mitigation_damage = attack_base_ad * (1.75 + attack_bonus_crit_damage) * (1 + percentage_AD_increase) + attack_bonus_ad * (1.75 + attack_bonus_crit_damage) * (1 + percentage_AD_increase) * (1 + percentage_bonus_AD_increase) + flat_AD_increase
-    defense_armor = defense_base_armor + defense_bonus_armor
-    if defense_armor < 0:
-        return physical_damage_after_negative_armor(pre_mitigation_damage, defense_armor)
-    else:
-        attack_flat_armor_pen = attack_lethality * (0.6 + 0.4 * attack_level / 18)
-        armor_eq = defense_base_armor * attack_armor_pen_mult_factor + defense_bonus_armor * attack_armor_pen_mult_factor * attack_bonus_armor_pen_mult_factor
-        armor_eq = armor_eq - attack_flat_armor_pen
-        armor_eq = max(armor_eq, 0)
-        return physical_damage_after_positive_armor(pre_mitigation_damage, armor_eq)
+    return physical_damage_after_armor(pre_mitigation_damage, attack_lethality, attack_level,
+                                       attack_armor_pen_mult_factor, attack_bonus_armor_pen_mult_factor,
+                                       defense_base_armor, defense_bonus_armor)
+
 
 
 def avg_damage_empowered_auto_attack(attack_base_ad: float, attack_bonus_ad: float, attack_crit_chance: float,
@@ -138,12 +126,6 @@ def avg_damage_empowered_auto_attack(attack_base_ad: float, attack_bonus_ad: flo
     percentage armor reduction resulting from spells like Garen E, Trundle R, Olaf Q, Corki E, or items like Black Cleaver
     """
     pre_mitigation_damage = attack_base_ad * (1 + attack_crit_chance * (0.75 + attack_bonus_crit_damage)) * (1 + percentage_AD_increase) + attack_bonus_ad * (1 + attack_crit_chance * (0.75 + attack_bonus_crit_damage)) * (1 + percentage_AD_increase) * (1 + percentage_bonus_AD_increase) + flat_AD_increase
-    defense_armor = defense_base_armor + defense_bonus_armor
-    if defense_armor < 0:
-        return physical_damage_after_negative_armor(pre_mitigation_damage, defense_armor)
-    else:
-        attack_flat_armor_pen = attack_lethality * (0.6 + 0.4 * attack_level / 18)
-        armor_eq = defense_base_armor * attack_armor_pen_mult_factor + defense_bonus_armor * attack_armor_pen_mult_factor * attack_bonus_armor_pen_mult_factor
-        armor_eq = armor_eq - attack_flat_armor_pen
-        armor_eq = max(armor_eq, 0)
-        return physical_damage_after_positive_armor(pre_mitigation_damage, armor_eq)
+    return physical_damage_after_armor(pre_mitigation_damage, attack_lethality, attack_level,
+                                       attack_armor_pen_mult_factor, attack_bonus_armor_pen_mult_factor,
+                                       defense_base_armor, defense_bonus_armor)
