843de47e89708a2507f40f95508ef54d1455bea2
nbzpro <specialzhdav@gmail.com>
2022-11-18 00:44:58 +0100

    added sheen with passive effect and test

--- item.py
+++ item.py
@@ -1,5 +1,6 @@
 from data_parser import ALL_ITEM_STATS
-
+from damage import damage_after_positive_resistance
+from champion import Dummy
 
 class BaseItem:
     def __init__(self, item_name: str):
@@ -12,3 +13,14 @@ class DoranBlade(BaseItem):
     def __init__(self, **kwargs):
         super().__init__(item_name=__class__.item_name, **kwargs)
 
+class Sheen(BaseItem):
+    item_name = "Sheen"
+
+    def __init__(self, **kwargs):
+        super().__init__(item_name=__class__.item_name, **kwargs)
+
+    def spellblade(self, owner_champion, enemy_champion):
+        """Calculates the bonus damage dealt with an autoattack : 100% of base AD """
+        if isinstance(enemy_champion, Dummy):
+            return damage_after_positive_resistance(owner_champion.attack_damage, enemy_champion.bonus_armor)
+        return damage_after_positive_resistance(owner_champion.attack_damage, enemy_champion.armor)
--- tests/test_item.py
+++ tests/test_item.py
@@ -1,9 +1,18 @@
 import math
 
 from item import DoranBlade
+from item import Sheen
+from champion import Ahri, Annie
+from champion import Dummy
+from damage import damage_after_positive_resistance
 
 
 def test_DoranBlade():
     doranblade = DoranBlade()
     assert doranblade.stats == {'attack_damage': 8, 'health': 80, "gold": 450}
 
+def test_Sheen():
+    sheen = Sheen()
+    annie = Annie()
+    dummy=Dummy(1000,50,50)
+    assert sheen.spellblade(annie, dummy) ==  damage_after_positive_resistance(annie.attack_damage, dummy.bonus_armor)
