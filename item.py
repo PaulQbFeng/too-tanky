d510cf909b1bcd3cd7650b5dc46d8880b28babcf
PaulQbFeng <dofxpaul@gmail.com>
2022-11-19 18:34:03 +0100

    refactor attribute setting

--- item.py
+++ item.py
@@ -2,8 +2,16 @@ from data_parser import ALL_ITEM_STATS
 
 
 class BaseItem:
+    """
+    Base class to represent an item. It is initialized with the base stats of the item.
+    Additional effects passive/active are handled in the children Champion specific classes.
+    """
     def __init__(self, item_name: str):
-        self.stats = ALL_ITEM_STATS[item_name]
+        self.update_stat(item_name)
+
+    def update_stat(self, item_name):
+        for stat_name, stat_value in ALL_ITEM_STATS[item_name].items():
+            setattr(self, stat_name, stat_value)
 
 
 class DoranBlade(BaseItem):
