44e0d98636c8b939b71bce44b5a962a577991ba2
PaulQbFeng <dofxpaul@gmail.com>
2022-11-20 00:47:27 +0100

    correct_test

--- tests/test_damage.py
+++ tests/test_damage.py
@@ -66,55 +66,55 @@ def tests_normal_auto_attack_cait_dummy_10():
     Same against a dummy with 10 bonus armor and 10 bonus mr (to test what happens when lethality is higher than armor)
     Caitlyn has one adaptive force rune which gives 5.4 attack damage
     """
-    dummy_10 = Dummy(1000, 10, 10)
+    dummy_10 = Dummy(health=1000, bonus_resistance=10)
     # Caitlyn level 1 with a doran's blade
     # No headshot no crit
     assert round(damage_auto_attack(base_attack_damage=62, bonus_attack_damage=13.4, lethality=0, attacker_level=1,
-                                    armor_pen_mult_factor=1, bonus_armor_pen_mult_factor=1, base_armor=dummy_10.armor,
-                                    bonus_armor=dummy_10.bonus_armor, damage_modifier_flat=0,
+                                    armor_pen_mult_factor=1, bonus_armor_pen_mult_factor=1, base_armor=dummy_10.orig_base_stats["armor"],
+                                    bonus_armor=dummy_10.orig_bonus_stats["armor"], damage_modifier_flat=0,
                                     damage_modifier_percent_mult_factor=1, crit=False, crit_damage=0)) == 69
     # Caitlyn level 11 with an infinity edge
     # No headshot no crit
     assert round(damage_auto_attack(base_attack_damage=95.345, bonus_attack_damage=75.4, lethality=0, attacker_level=11,
-                                    armor_pen_mult_factor=1, bonus_armor_pen_mult_factor=1, base_armor=dummy_10.armor,
-                                    bonus_armor=dummy_10.bonus_armor, damage_modifier_flat=0,
+                                    armor_pen_mult_factor=1, bonus_armor_pen_mult_factor=1, base_armor=dummy_10.orig_base_stats["armor"],
+                                    bonus_armor=dummy_10.orig_bonus_stats["armor"], damage_modifier_flat=0,
                                     damage_modifier_percent_mult_factor=1, crit=False, crit_damage=0)) == 155
     # No headshot with crit
     assert round(damage_auto_attack(base_attack_damage=95.345, bonus_attack_damage=75.4, lethality=0, attacker_level=11,
-                                    armor_pen_mult_factor=1, bonus_armor_pen_mult_factor=1, base_armor=dummy_10.armor,
-                                    bonus_armor=dummy_10.bonus_armor, damage_modifier_flat=0,
+                                    armor_pen_mult_factor=1, bonus_armor_pen_mult_factor=1, base_armor=dummy_10.orig_base_stats["armor"],
+                                    bonus_armor=dummy_10.orig_bonus_stats["armor"], damage_modifier_flat=0,
                                     damage_modifier_percent_mult_factor=1, crit=True, crit_damage=0)) == 272
     # Caitlyn level 11 with an infinity edge and a lord dominik's regards
     # No headshot no crit
     assert round(damage_auto_attack(base_attack_damage=95.345, bonus_attack_damage=105.4, lethality=0, attacker_level=11,
-                                    armor_pen_mult_factor=0.7, bonus_armor_pen_mult_factor=1, base_armor=dummy_10.armor,
-                                    bonus_armor=dummy_10.bonus_armor, damage_modifier_flat=0,
+                                    armor_pen_mult_factor=0.7, bonus_armor_pen_mult_factor=1, base_armor=dummy_10.orig_base_stats["armor"],
+                                    bonus_armor=dummy_10.orig_bonus_stats["armor"], damage_modifier_flat=0,
                                     damage_modifier_percent_mult_factor=1, crit= False, crit_damage=0)) == 188
     # No headshot with crit
     assert round(damage_auto_attack(base_attack_damage=95.345, bonus_attack_damage=105.4, lethality=0, attacker_level=11,
-                                    armor_pen_mult_factor=0.7, bonus_armor_pen_mult_factor=1, base_armor=dummy_10.armor,
-                                    bonus_armor=dummy_10.bonus_armor, damage_modifier_flat=0,
+                                    armor_pen_mult_factor=0.7, bonus_armor_pen_mult_factor=1, base_armor=dummy_10.orig_base_stats["armor"],
+                                    bonus_armor=dummy_10.orig_bonus_stats["armor"], damage_modifier_flat=0,
                                     damage_modifier_percent_mult_factor=1, crit=True, crit_damage=0)) == 328
     # Caitlyn level 11 with a duskblade of draktharr (and cloak of agility for crits)
     # No headshot no crit no duskblade effect
     assert round(damage_auto_attack(base_attack_damage=95.345, bonus_attack_damage=65.4, lethality=18, attacker_level=11,
-                                    armor_pen_mult_factor=1, bonus_armor_pen_mult_factor=1, base_armor=dummy_10.armor,
-                                    bonus_armor=dummy_10.bonus_armor, damage_modifier_flat=0,
+                                    armor_pen_mult_factor=1, bonus_armor_pen_mult_factor=1, base_armor=dummy_10.orig_base_stats["armor"],
+                                    bonus_armor=dummy_10.orig_bonus_stats["armor"], damage_modifier_flat=0,
                                     damage_modifier_percent_mult_factor=1, crit=False, crit_damage=0)) == 161
     # No headshot with crit no duskblade effect
     assert round(damage_auto_attack(base_attack_damage=95.345, bonus_attack_damage=65.4, lethality=18, attacker_level=11,
-                                    armor_pen_mult_factor=1, bonus_armor_pen_mult_factor=1, base_armor=dummy_10.armor,
-                                    bonus_armor=dummy_10.bonus_armor, damage_modifier_flat=0,
+                                    armor_pen_mult_factor=1, bonus_armor_pen_mult_factor=1, base_armor=dummy_10.orig_base_stats["armor"],
+                                    bonus_armor=dummy_10.orig_bonus_stats["armor"], damage_modifier_flat=0,
                                     damage_modifier_percent_mult_factor=1, crit=True, crit_damage=0)) == 281
     # Caitlyn level 11 with a duskblade of draktharr and lord dominik's regards
     # No headshot no crit no duskblade effect
     assert round(damage_auto_attack(base_attack_damage=95.345, bonus_attack_damage=95.4, lethality=18, attacker_level=11,
-                                    armor_pen_mult_factor=0.7, bonus_armor_pen_mult_factor=1, base_armor=dummy_10.armor,
-                                    bonus_armor=dummy_10.bonus_armor, damage_modifier_flat=0,
+                                    armor_pen_mult_factor=0.7, bonus_armor_pen_mult_factor=1, base_armor=dummy_10.orig_base_stats["armor"],
+                                    bonus_armor=dummy_10.orig_bonus_stats["armor"], damage_modifier_flat=0,
                                     damage_modifier_percent_mult_factor=1, crit=False, crit_damage=0)) == 191
     # No headshot with crit no duskblade effect
     assert round(damage_auto_attack(base_attack_damage=95.345, bonus_attack_damage=95.4, lethality=18, attacker_level=11,
-                                    armor_pen_mult_factor=0.7, bonus_armor_pen_mult_factor=1, base_armor=dummy_10.armor,
-                                    bonus_armor=dummy_10.bonus_armor, damage_modifier_flat=0,
+                                    armor_pen_mult_factor=0.7, bonus_armor_pen_mult_factor=1, base_armor=dummy_10.orig_base_stats["armor"],
+                                    bonus_armor=dummy_10.orig_bonus_stats["armor"], damage_modifier_flat=0,
                                     damage_modifier_percent_mult_factor=1, crit=True, crit_damage=0)) == 334
 
