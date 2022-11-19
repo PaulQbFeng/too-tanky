9cf1b9fb58950cae2c7b9c762d57d1cfba176886
LinSun00 <91116220+LinSun00@users.noreply.github.com>
2022-11-19 23:21:55 +0100

    add Stats class

--- champion.py
+++ champion.py
@@ -1,8 +1,59 @@
-from damage import damage_after_positive_resistance
+from damage import damage_auto_attack
 from data_parser import ALL_CHAMPION_BASE_STATS, SCALING_STAT_NAMES
 
 
 # TODO: Might be a good opportunity to use abstract class for base champion
+class Stats:
+    """
+    Champions, items, and rune shards can all be considered to have stats.
+    Stats in league all have a fixed way of being stacked, hence why it makes sense to define a class.
+    """
+    def __init__(self):
+        self.health = 0
+        self.mana = 0
+        self.health_regen = 0
+        self.mana_regen = 0
+        self.attack_damage = 0
+        self.ability_power = 0
+        self.attack_speed = 0
+        self.armor = 0
+        self.magic_resist = 0
+        self.lethality = 0
+        self.armor_pen_percent = 0
+        self.bonus_armor_pen_percent = 0
+        self.magic_pen_flat = 0
+        self.magic_pen_percent = 0
+        self.crit_chance = 0
+        self.crit_damage = 0
+        self.life_steal = 0
+        self.omni_vamp = 0
+        self.spell_vamp = 0
+
+    def __str__(self):
+        return str(self.__class__) + ": " + str(self.__dict__)
+
+    def add_stats(self, stats):
+        self.health += stats.health
+        self.mana += stats.mana
+        self.health_regen += stats.health_regen
+        self.mana_regen += stats.mana_regen
+        self.attack_damage += stats.attack_damage
+        self.ability_power += stats.ability_power
+        self.attack_speed += stats.attack_speed
+        self.armor += stats.armor
+        self.magic_resist += stats.magic_resist
+        self.lethality += stats.lethality
+        self.armor_pen_percent = 1 - (1 - self.armor_pen_percent) * (1 - stats.armor_pen_percent)
+        self.bonus_armor_pen_percent = 1 - (1 - self.bonus_armor_pen_percent) * (1 - stats.bonus_armor_pen_percent)
+        self.magic_pen_flat += stats.magic_pen_flat
+        self.magic_pen_percent = 1 - (1 - self.magic_pen_percent) * (1 - stats.magic_pen_percent)
+        self.crit_chance += stats.crit_chance
+        self.crit_damage += stats.crit_damage
+        self.life_steal += stats.life_steal
+        self.omni_vamp += stats.omni_vamp
+        self.spell_vamp += stats.spell_vamp
+
+
 class BaseChampion:
     """
     Base class to represent a champion. It is initialized with level 1 stats and have traits shared
@@ -17,7 +68,25 @@ class BaseChampion:
     def __init__(self, champion_name: str, level: int = 1):
         assert isinstance(level, int) and 1 <= level <= 18, "Champion level should be in the [1,18] range"
         self.level = level
+        self.orig_base_stats = Stats()
+        self.orig_bonus_stats = Stats()
+        self.base_stats = Stats()
+        self.bonus_stats = Stats()
         self.update_stat_from_level(ALL_CHAMPION_BASE_STATS[champion_name])
+
+    @property
+    def orig_total_stats(self):
+        stats = Stats()
+        stats.add_stats(self.orig_base_stats)
+        stats.add_stats(self.orig_bonus_stats)
+        return stats
+
+    @property
+    def total_stats(self):
+        stats = Stats()
+        stats.add_stats(self.base_stats)
+        stats.add_stats(self.bonus_stats)
+        return stats
 
     def update_stat_from_level(self, champion_stats):
         """Takes all the base stats from the input dictionary and create the corresponding attributes in the instance"""
@@ -27,10 +96,10 @@ class BaseChampion:
 
             return base + mean_growth_perlevel * (level - 1) * (0.7025 + 0.0175 * (level - 1))
 
-        def calculate_stat_from_level(base_stats: dict, stat_name: str, level: int):
+        def calculate_stat_from_level(all_base_stats: dict, stat_name: str, level: int):
             """Flat scaling for all stats except for attack speed"""
-            stat = base_stats[stat_name]
-            mean_growth_perlevel = base_stats[stat_name + "_perlevel"]
+            stat = all_base_stats[stat_name]
+            mean_growth_perlevel = all_base_stats[stat_name + "_perlevel"]
             if stat_name == "attack_speed":
                 # attack speed scaling is in % instead of flat. Base increase level 1 is considered to be 0 %.
                 percentage_increase = calculate_flat_stat_from_level(0, mean_growth_perlevel, level)
@@ -38,11 +107,20 @@ class BaseChampion:
             return calculate_flat_stat_from_level(stat, mean_growth_perlevel, level)
 
         for stat_name in SCALING_STAT_NAMES:
-            self.__dict__[stat_name] = calculate_stat_from_level(champion_stats, stat_name, self.level)
+            setattr(self.orig_base_stats, stat_name, calculate_stat_from_level(champion_stats, stat_name, self.level))
 
-    def auto_attack(self, enemy_champion):
+    def normal_auto_attack(self, enemy_champion):
         """Calculates the damage dealt to an enemy champion with an autoattack"""
-        return damage_after_positive_resistance(self.attack_damage, enemy_champion.armor)
+        return damage_auto_attack(self.orig_base_stats.attack_damage, self.orig_bonus_stats.attack_damage,
+                                  self.orig_total_stats.lethality, self.level, self.orig_total_stats.armor_pen_percent,
+                                  self.orig_total_stats.bonus_armor_pen_percent, enemy_champion.orig_base_stats.armor,
+                                  enemy_champion.orig_bonus_stats.armor, 0, 1, False, 0)
+
+    def crit_auto_attack(self, enemy_champion):
+        return damage_auto_attack(self.orig_base_stats.attack_damage, self.orig_bonus_stats.attack_damage,
+                                  self.orig_total_stats.lethality, self.level, self.orig_total_stats.armor_pen_percent,
+                                  self.orig_total_stats.bonus_armor_pen_percent, enemy_champion.orig_base_stats.armor,
+                                  enemy_champion.orig_bonus_stats.armor, 0, 1, True, 0)
 
 
 # Dummy class for tests in practice tool.
@@ -52,11 +130,13 @@ class Dummy:
         assert bonus_armor % 10 == 0
         assert health % 100 == 0
         assert health <= 10000
-        self.health = health
-        self.armor = 0
-        self.magic_resist = 0
-        self.bonus_armor = bonus_armor
-        self.bonus_magic_resist = bonus_magic_resist
+        self.orig_base_stats = Stats()
+        self.orig_bonus_stats = Stats()
+        self.base_stats = Stats()
+        self.bonus_stats = Stats()
+        self.orig_base_stats.health = health
+        self.orig_bonus_stats.armor = bonus_armor
+        self.orig_bonus_stats.magic_resist = bonus_magic_resist
 
 
 # Each champion has its own class as their spells have different effects.
