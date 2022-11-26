c252c85458267d9dd9ef4adbcb946e4c63901967
nbzpro <specialzhdav@gmail.com>
2022-11-26 23:11:19 +0100

    malphite q and amplifying tome

--- item.py
+++ item.py
@@ -116,7 +116,7 @@ class SeryldaGrudge(BaseItem): #missing passive, ability haste
 
     def __init__(self, **kwargs):
         super().__init__(item_name=__class__.item_name, **kwargs)
-        self.stats["armor_pen_percent"] = 30
+        self.stats.add("armor_pen_percent", 30)
 
         
 class YoumuuGhostblade(BaseItem): #missing passive, active, ability haste
@@ -124,8 +124,13 @@ class YoumuuGhostblade(BaseItem): #missing passive, active, ability haste
 
     def __init__(self, **kwargs):
         super().__init__(item_name=__class__.item_name, **kwargs)
-        self.stats["lethality"] = 18
+        self.stats.add("lethality", 18)
 
+class AmplifyingTome(BaseItem):
+    item_name = "Amplifying Tome"
+
+    def __init__(self, **kwargs):
+        super().__init__(item_name=__class__.item_name, **kwargs)
 
 
 class BlastingWand(BaseItem):
