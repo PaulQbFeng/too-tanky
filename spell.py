caa50cb04709c41f0de4fa277846dac80ddc1535
PaulQbFeng <dofxpaul@gmail.com>
2022-11-26 11:22:58 +0100

    move spell spec init with level in BaseSpell

--- spell.py
+++ spell.py
@@ -1,15 +1,18 @@
 from data_parser import ALL_CHAMPION_SPELLS
 
-NORMAL_SPELL_KEY = ["q", "w", "e"]
-
 
 class BaseSpell:
-    def __init__(self, champion_name, spell_key):
+    def __init__(self, champion_name, spell_key, level):
         self.champion_name = champion_name
         self.spell_key = spell_key
         spell_specs = ALL_CHAMPION_SPELLS[champion_name][spell_key].copy()
         for name, value in spell_specs.items():
             setattr(self, name, value)
+
+        self.level = level
+        self.range = self.range[level - 1]
+        self.cost = self.cost[level - 1]
+        self.cooldown = self.cooldown[level - 1]
 
     def print_specs(self):
         """pretty print the stats"""
@@ -21,18 +24,14 @@ class QAnnie(BaseSpell):
     spell_key = "q"
 
     def __init__(self, level):
-        super().__init__(champion_name=__class__.champion_name, spell_key=__class__.spell_key)
+        super().__init__(champion_name=__class__.champion_name, spell_key=__class__.spell_key, level=level)
 
-        if self.spell_key in NORMAL_SPELL_KEY:
+        if self.spell_key in ["q", "w", "e"]:
             self.nature = "normal"
         else:
             self.nature = "ulti"
-
-        self.base_damage_per_level = [80, 115, 150, 185, 220]
+        
         self.damage_type = "magical"
-        self.level = level
-        self.range = self.range[level - 1]
-        self.cost = self.cost[level - 1]
-        self.cooldown = self.cooldown[level - 1]
-        self.ratio = self.ratios[0]
+        self.base_damage_per_level = [80, 115, 150, 185, 220]
         self.base_spell_damage = self.base_damage_per_level[level - 1]
+        self.ratio = self.ratios[0] # ratios is a list of 2 values, maybe it's ratio for 2 different damage type idk 
