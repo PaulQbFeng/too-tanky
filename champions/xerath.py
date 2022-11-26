8532f71ef49c3566d4f529f5d42be850836390fb
LinSun00 <91116220+LinSun00@users.noreply.github.com>
2022-11-27 00:13:44 +0100

    xerath all spells

--- champions/xerath.py
+++ champions/xerath.py
@@ -48,6 +48,43 @@ class Xerath(BaseChampion):
         else:
             return post_mtg_dmg * 1.667
 
+    def spell_e(self, level, enemy_champion):
+        self.e = EXerath(level=level)
+
+        pre_mtg_dmg = pre_mitigation_spell_damage(
+            base_spell_damage=self.e.base_spell_damage,
+            ratio=self.e.ratio,
+            base_offensive_stats=self.orig_base_stats.get("ability_power", 0),
+            bonus_offensive_stats=self.orig_bonus_stats.get("ability_power", 0),
+        )
+
+        post_mtg_dmg = damage_after_resistance(
+            pre_mitigation_damage=pre_mtg_dmg,
+            base_resistance=enemy_champion.orig_base_stats.magic_resist,
+            bonus_resistance=enemy_champion.orig_bonus_stats.magic_resist,
+            flat_resistance_pen=enemy_champion.orig_bonus_stats.get("flat_magic_resist_pen", 0),
+        )
+        return post_mtg_dmg
+
+    def spell_r(self, level: int, enemy_champion: BaseChampion, nb_hit):
+        self.r = RXerath(level=level)
+        assert 0 <= nb_hit <= self.r.recast_per_level[level-1]
+
+        pre_mtg_dmg = pre_mitigation_spell_damage(
+            base_spell_damage=self.r.base_spell_damage,
+            ratio=self.r.ratio,
+            base_offensive_stats=self.orig_base_stats.get("ability_power", 0),
+            bonus_offensive_stats=self.orig_bonus_stats.get("ability_power", 0),
+        )
+
+        post_mtg_dmg = damage_after_resistance(
+            pre_mitigation_damage=pre_mtg_dmg,
+            base_resistance=enemy_champion.orig_base_stats.magic_resist,
+            bonus_resistance=enemy_champion.orig_bonus_stats.magic_resist,
+            flat_resistance_pen=enemy_champion.orig_bonus_stats.get("flat_magic_resist_pen", 0),
+        )
+        return post_mtg_dmg * nb_hit
+
 
 class QXerath(BaseSpell):
     champion_name = "Xerath"
@@ -83,3 +120,40 @@ class WXerath(BaseSpell):
         self.base_damage_per_level = [60, 95, 130, 165, 200]
         self.base_spell_damage = self.base_damage_per_level[level - 1]
         self.ratio = 0.6
+
+
+class EXerath(BaseSpell):
+    champion_name = "Xerath"
+    spell_key = "e"
+
+    def __init__(self, level):
+        super().__init__(champion_name=__class__.champion_name, spell_key=__class__.spell_key, level=level)
+
+        if self.spell_key in ["q", "w", "e"]:
+            self.nature = "normal"
+        else:
+            self.nature = "ulti"
+
+        self.damage_type = "magical"
+        self.base_damage_per_level = [80, 110, 140, 170, 200]
+        self.base_spell_damage = self.base_damage_per_level[level - 1]
+        self.ratio = 0.45
+
+
+class RXerath(BaseSpell):
+    champion_name = "Xerath"
+    spell_key = "r"
+
+    def __init__(self, level):
+        super().__init__(champion_name=__class__.champion_name, spell_key=__class__.spell_key, level=level)
+
+        if self.spell_key in ["q", "w", "e"]:
+            self.nature = "normal"
+        else:
+            self.nature = "ulti"
+
+        self.damage_type = "magical"
+        self.base_damage_per_level = [200, 250, 300]
+        self.recast_per_level = [3, 4, 5]
+        self.base_spell_damage = self.base_damage_per_level[level - 1]
+        self.ratio = 0.45
--- tests/test_xerath.py
+++ tests/test_xerath.py
@@ -1,4 +1,4 @@
-from champions.xerath import Xerath, QXerath
+from champions.xerath import Xerath
 from champion import Dummy
 from item import BlastingWand
 
@@ -23,6 +23,7 @@ def test_dummy_0_res():
     xerath.equip_item(blastingwand)
     assert round(xerath.spell_q(5, dummy)) == 264
 
+
 def test_dummy_30_res():
     dummy = Dummy(1000, 30)
     xerath = Xerath(level=1)
@@ -34,24 +35,41 @@ def test_dummy_30_res():
     assert round(xerath.spell_q(1, dummy)) == 80
     assert round(xerath.spell_w(1, dummy, False)) == 65
     assert round(xerath.spell_w(1, dummy, True)) == 108
+    assert round(xerath.spell_e(1, dummy)) == 75
+    xerath = Xerath(level=6)
+    xerath.equip_item(blastingwand)
+    assert round(xerath.spell_r(1, dummy, 0)) == 0
+    assert round(xerath.spell_r(1, dummy, 1)) == 168
+    assert round(xerath.spell_r(1, dummy, 2)) == 335
+    assert round(xerath.spell_r(1, dummy, 3)) == 503
     xerath = Xerath(level=3)
     xerath.equip_item(blastingwand)
     assert round(xerath.spell_q(2, dummy)) == 111
     assert round(xerath.spell_w(2, dummy, False)) == 92
     assert round(xerath.spell_w(2, dummy, True)) == 153
+    assert round(xerath.spell_e(2, dummy)) == 98
+    xerath = Xerath(level=11)
+    xerath.equip_item(blastingwand)
+    assert round(xerath.spell_r(2, dummy, 1)) == 206
     xerath = Xerath(level=5)
     xerath.equip_item(blastingwand)
     assert round(xerath.spell_q(3, dummy)) == 142
     assert round(xerath.spell_w(3, dummy, False)) == 118
     assert round(xerath.spell_w(3, dummy, True)) == 197
+    assert round(xerath.spell_e(3, dummy)) == 122
     xerath = Xerath(level=7)
     xerath.equip_item(blastingwand)
     assert round(xerath.spell_q(4, dummy)) == 172
     assert round(xerath.spell_w(4, dummy, False)) == 145
     assert round(xerath.spell_w(4, dummy, True)) == 242
+    assert round(xerath.spell_e(4, dummy)) == 145
+    xerath = Xerath(level=16)
+    xerath.equip_item(blastingwand)
+    assert round(xerath.spell_r(3, dummy, 1)) == 245
     xerath = Xerath(level=9)
     xerath.equip_item(blastingwand)
     assert round(xerath.spell_q(5, dummy)) == 203
     assert round(xerath.spell_w(5, dummy, False)) == 172
     assert round(xerath.spell_w(5, dummy, True)) == 287
+    assert round(xerath.spell_e(5, dummy)) == 168
 
