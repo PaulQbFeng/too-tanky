e699754e00e9000196cf0506edb474b26a0bd920
PaulQbFeng <dofxpaul@gmail.com>
2022-11-26 12:40:59 +0100

    Add docstring for spell

--- spell.py
+++ spell.py
@@ -2,6 +2,19 @@ from data_parser import ALL_CHAMPION_SPELLS
 
 
 class BaseSpell:
+    """
+    ALL_CHAMPION_SPELLS["Kog\'Maw"]["e"] contains:
+        {
+            'name': 'Void Ooze',
+            'range': [1200.0, 1200.0, 1200.0, 1200.0, 1200.0],
+            'cost': [60.0, 70.0, 80.0, 90.0, 100.0],
+            'cooldown': [12.0, 12.0, 12.0, 12.0, 12.0],
+            'ratios': [0.7, 0.0],
+            'max_level': 0
+        }
+    Not all spell specifications are included in the data file which means there is a need to double check
+    the current specs + add the missing ones inside the subclass of BaseSpell. 
+    """
     def __init__(self, champion_name, spell_key, level):
         self.champion_name = champion_name
         self.spell_key = spell_key
@@ -34,4 +47,4 @@ class QAnnie(BaseSpell):
         self.damage_type = "magical"
         self.base_damage_per_level = [80, 115, 150, 185, 220]
         self.base_spell_damage = self.base_damage_per_level[level - 1]
-        self.ratio = self.ratios[0] # ratios is a list of 2 values, maybe it's ratio for 2 different damage type idk 
+        self.ratio = self.ratios[0] # ratios is a list of 2 values, maybe it's ratio for 2 different damage type 
