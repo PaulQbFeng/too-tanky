16e5dd9978d8ef32e5f7844f3b2d50b9248696c7
paulqbfeng <dofxpaul@gmail.com>
2022-11-27 01:12:21 +0100

    refactor xerath first test

--- tests/champions/test_xerath.py
+++ tests/champions/test_xerath.py
@@ -1,27 +1,17 @@
-from champions.xerath import Xerath
 from champion import Dummy
+from champions.xerath import Xerath
 from item import BlastingWand
 
 
 def test_dummy_0_res():
     dummy = Dummy(1000, 0)
-    xerath = Xerath(level=1)
-    blastingwand = BlastingWand()
+    xerath = Xerath(level=18)
     assert round(xerath.spell_q(1, dummy)) == 70
-    xerath.equip_item(blastingwand)
-    assert round(xerath.spell_q(1, dummy)) == 104
-    xerath = Xerath(level=3)
-    xerath.equip_item(blastingwand)
-    assert round(xerath.spell_q(2, dummy)) == 144
-    xerath = Xerath(level=5)
-    xerath.equip_item(blastingwand)
-    assert round(xerath.spell_q(3, dummy)) == 184
-    xerath = Xerath(level=7)
-    xerath.equip_item(blastingwand)
-    assert round(xerath.spell_q(4, dummy)) == 224
-    xerath = Xerath(level=9)
-    xerath.equip_item(blastingwand)
-    assert round(xerath.spell_q(5, dummy)) == 264
+
+    xerath.equip_item(BlastingWand())
+    damage_per_level = [104, 144, 184, 224, 264]
+    for spell_level in range(1, 6):
+        assert round(xerath.spell_q(spell_level, dummy)) == damage_per_level[spell_level - 1]
 
 
 def test_dummy_30_res():
@@ -72,4 +62,3 @@ def test_dummy_30_res():
     assert round(xerath.spell_w(5, dummy, False)) == 172
     assert round(xerath.spell_w(5, dummy, True)) == 287
     assert round(xerath.spell_e(5, dummy)) == 168
-
