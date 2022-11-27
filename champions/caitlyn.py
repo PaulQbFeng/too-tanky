413a4bd42b7427a584d57529ea4b50e1437cf8b8
LinSun00 <91116220+LinSun00@users.noreply.github.com>
2022-11-27 03:01:44 +0100

    Caitlyn Q

--- champions/caitlyn.py
+++ champions/caitlyn.py
@@ -1,5 +1,6 @@
 from champion import BaseChampion
-from damage import damage_physical_auto_attack
+from damage import damage_physical_auto_attack, pre_mitigation_spell_damage, damage_after_resistance
+from spell import BaseSpell
 
 
 class Caitlyn(BaseChampion):
@@ -44,3 +45,38 @@ class Caitlyn(BaseChampion):
             )
             self.auto_attack_count = 0
         return damage
+
+    def spell_q(self, level, enemy_champion):
+        self.q = QCaitlyn(level=level)
+
+        pre_mtg_dmg = pre_mitigation_spell_damage(
+            base_spell_damage=self.q.base_spell_damage,
+            ratio=self.q.ratio,
+            base_offensive_stats=self.orig_base_stats.get("attack_damage", 0),
+            bonus_offensive_stats=self.orig_bonus_stats.get("attack_damage", 0),
+        )
+
+        post_mtg_dmg = damage_after_resistance(
+            pre_mitigation_damage=pre_mtg_dmg,
+            base_resistance=enemy_champion.orig_base_stats.armor,
+            bonus_resistance=enemy_champion.orig_bonus_stats.armor,
+        )
+        return post_mtg_dmg
+
+
+class QCaitlyn(BaseSpell):
+    champion_name = "Annie"
+    spell_key = "q"
+
+    def __init__(self, level):
+        super().__init__(champion_name=__class__.champion_name, spell_key=__class__.spell_key, level=level)
+
+        if self.spell_key in ["q", "w", "e"]:
+            self.nature = "normal"
+        else:
+            self.nature = "ulti"
+
+        self.damage_type = "physical"
+        self.base_damage_per_level = [50, 90, 130, 170, 210]
+        self.base_spell_damage = self.base_damage_per_level[level - 1]
+        self.ratios = [1.25, 1.45, 1.65, 1.85, 2.05]
